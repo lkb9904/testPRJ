4ec4d9964e98ed3c9f3a7666c4d930397f802cfb
LKBKB <shs7556@gmail.com>
2026-03-31 22:56:50 +0900

    수정6

--- supabase/migrations/20260331180000_pickup_locations_anon_select.sql
+++
@@ -1,7 +0,0 @@
--- 비로그인 방문자도 활성 픽업 장소 목록을 볼 수 있도록 (고객용 /pickup 페이지)
-DROP POLICY IF EXISTS "pickup_locations_select_anon_active" ON public.pickup_locations;
-
-CREATE POLICY "pickup_locations_select_anon_active"
-  ON public.pickup_locations FOR SELECT
-  TO anon
-  USING (is_active = true);
